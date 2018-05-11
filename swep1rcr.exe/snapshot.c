a401c881f14c609578e04b44ea1873150da7cf50
Jannik Vogel <email@jannikvogel.de>
2018-05-11 17:06:49 +0200

    Comment sub_410480

--- swep1rcr.exe/snapshot.c
+++ swep1rcr.exe/snapshot.c
@@ -7,19 +7,29 @@ signed int __cdecl sub_410480(int a1)
 {
   int v1; // ecx
   FILE *v2; // eax
-  FILE *v3; // esi
   char v5; // [esp+8h] [ebp-50h]
 
-  do
-  {
-    v1 = dword_4D79E4++;
+  // Search for a free screenshot index
+  do {
+
+    // Get index for this screenshot and construct filename
+    int v1 = dword_4D79E4++;
     sprintf(&v5, aS03dBmp, a1, v1);
+
+    // Try to open the file (for read access)
     v2 = fopen(&v5, aRb);
-    v3 = v2;
-    if ( !v2 )
+
+    // If we were unable to open it, assume that it doesn't exist.
+    // This means we have found a free index, so we can leave the loop
+    if (v2 == NULL) {
       break;
+    }
+
+    // If we opened the file successfully, close it
     fclose(v2);
-  }
-  while ( v3 );
+
+  } while ( v2 );
+
+  // Take the actual screenshot now
   return sub_489D20((int)&v5);
 }
